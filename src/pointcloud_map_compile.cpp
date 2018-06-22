b1904a03fd8721e186ab77a098689f6c24b844fc
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2018-06-22 12:02:31 +0530

    working model

--- src/pointcloud_map_compile.cpp
+++ src/pointcloud_map_compile.cpp
@@ -1,14 +1,16 @@
 #include "ros/ros.h"
-#include <tf/transform_listener.h>
-#include <pcl_ros/transforms.h>
-#include "sensor_msgs/PointCloud2.h"
-#include <pcl/point_cloud.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_types.h>
-#include <pcl/io/pcd_io.h>
 #include <iostream>
 #include <math.h>
-
+#include <tf/transform_listener.h>
+#include "sensor_msgs/PointCloud2.h"
+#include <pcl_ros/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/conversions.h>
 
 class PointCloudMap
 {
@@ -33,15 +35,24 @@ public:
   void transformCallback(const sensor_msgs::PointCloud2::ConstPtr& pcl_in) 
   { 
       ROS_INFO("Cloud received"); 
-      sensor_msgs::PointCloud2 pcl_out; 
-      tf_listener->waitForTransform("/map", (*pcl_in).header.frame_id,(*pcl_in).header.stamp, ros::Duration(5.0));  
-   
-      pcl_ros::transformPointCloud("/map", *pcl_in, pcl_transform, *tf_listener);
-      pcl_out  = filter(pcl_transform);
+      sensor_msgs::PointCloud2 pcl_out;
+      pcl::PointCloud<pcl::PointXYZRGB> test_cloud;
+ //     tf_listener->waitForTransform("/map", (*pcl_in).header.frame_id,(*pcl_in).header.stamp, ros::Duration(5.0));  
+      pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2 ());
+      pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2 ());
+      pcl_conversions::toPCL(*pcl_in,*cloud);
+      pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+      sor.setInputCloud (cloud);
+      sor.setLeafSize (0.05f, 0.05f, 0.05f);
+      sor.filter (*cloud_filtered);
+      pcl::fromPCLPointCloud2(*cloud_filtered,test_cloud);
+      pcl::toROSMsg(test_cloud,pcl_out);
+      ROS_INFO("Downsampled points %d",(pcl_out.width));
+      pcl_out = filter(pcl_out);
+      pcl_ros::transformPointCloud("/map", pcl_out, pcl_transform, *tf_listener);
+     
       ROS_INFO("Transformed Cloud");
-      pcl_out.header.frame_id = pcl_transform.header.frame_id;
-      pcl_out.header.stamp = pcl_transform.header.stamp;
-      pub2.publish(pcl_out);
+      pub2.publish(pcl_transform);
       
       if(q==0)
       {
@@ -61,7 +72,7 @@ public:
       pcl::PointCloud<pcl::PointXYZRGB> test_cloud;
       pcl::fromROSMsg(pcl_in,test_cloud);
       ROS_INFO("Inputcloud size is");
-      //ROS_INFO("%d",test_cloud.points.size ());
+      ROS_INFO("%ld",test_cloud.points.size ());
       float d, x , y, z;
       int j=0;
       filter_pcl_transform = test_cloud;
@@ -70,8 +81,8 @@ public:
         x = test_cloud.points[i].x;
         y = test_cloud.points[i].y;
         z = test_cloud.points[i].z;
-    	d = pow(x,2)+pow(y,2)+pow(z,2) ;
-    	if(d<5)
+    	d = sqrt(pow(x,2)+pow(y,2)+pow(z,2)) ;
+    	if(z<5)
     	{
    	   filter_pcl_transform.points[j].x = x;
    	   filter_pcl_transform.points[j].y = y;
